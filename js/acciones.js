c0bf345a40f755f9b179afe3e49056c53c67785b
jesuscruzbermudez <bermudezcruzjesus@gmail.com>
2019-04-29 21:48:45 -0500

    2

--- js/acciones.js
+++ js/acciones.js
@@ -1,14 +1,14 @@
 //JavaScript Document
 
-$(document).ready(function(e){
-	document.addEventListener("deviceready",function(){
+$(document).ready(function(e) {
+    document.addEventListener("deviceready",function(){
 		
 		$('#beep').tap(function(){
-			navigatior.notification.beep(1);
+			navigator.notification.beep(1);
 		});//tap beep
 		
 		$('#vibrar').tap(function(){
-			navigatior.notification.vibrate(1000);
+			navigator.notification.vibrate(1000);
 		});//tap vibrar
 		
 	},false); //deviceready
